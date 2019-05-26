88721a181e2cad8da725946e250d97dcb61fc21e
nick.stuivenberg <nick.stuivenberg@cgi.com>
2019-05-26 20:46:49 +0200

    opdracht07-03: Version 1.4: Did some extra refactoring

--- src/main/java/nl/hu/sie/bep/friendspammer/EmailSender.java
+++ src/main/java/nl/hu/sie/bep/friendspammer/EmailSender.java
@@ -14,29 +14,26 @@ import org.slf4j.LoggerFactory;
 
 public class EmailSender {
 
+	public static final String TEXT_HTML_CHARSET_UTF_8 = "text/html; charset=utf-8";
 	private static Logger logger = LoggerFactory.getLogger(EmailSender.class);
+
+	private static final String USERNAME = "c8b36f152fddae";
+	private static final String GEHEIM = "823d4a128104d1";
+
+	private static final String SMTP_HOST_PROPERTYNAME = "mail.smtp.host";
+	private static final String SMTP_PORT_PROPERTYNAME = "mail.smtp.port";
+	private static final String SMTP_AUTH_PROPERTYNAME = "mail.smtp.auth";
+	private static final String SMTP_HOST = "smtp.mailtrap.io";
+	private static final String SMTP_PORT = "2525";
+	private static final String SMTP_AUTH = "true";
 
 	private EmailSender(){}
 
 	public static void sendEmail(String subject, String to, String messageBody, boolean asHtml) {
 
-		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.mailtrap.io");
-		props.put("mail.smtp.port", "2525");
-		props.put("mail.smtp.auth", "true");
+		Session session = createSession();
 
-		String username = "c8b36f152fddae";
-		String geheim = "823d4a128104d1";
-
-		Session session = Session.getInstance(props,
-				new javax.mail.Authenticator() {
-					@Override
-					protected PasswordAuthentication getPasswordAuthentication() {
-						return new PasswordAuthentication(username, geheim);
-					}
-				});
 		try {
-
 			Message message = new MimeMessage(session);
 			message.setFrom(new InternetAddress("84854a3561-5e519e@inbox.mailtrap.io"));
 			message.setRecipients(Message.RecipientType.TO,
@@ -44,7 +41,7 @@ public class EmailSender {
 			message.setSubject(subject);
 
 			if (asHtml) {
-				message.setContent(messageBody, "text/html; charset=utf-8");
+				message.setContent(messageBody, TEXT_HTML_CHARSET_UTF_8);
 			} else {
 				message.setText(messageBody);
 			}
@@ -59,21 +56,8 @@ public class EmailSender {
 
 	public static void sendEmail(String subject, String[] toList, String messageBody, boolean asHtml) {
 
-		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.mailtrap.io");
-		props.put("mail.smtp.port", "2525");
-		props.put("mail.smtp.auth", "true");
+		Session session = createSession();
 
-		String username = "c8b36f152fddae";
-		String geheim = "823d4a128104d1";
-
-		Session session = Session.getInstance(props,
-				new javax.mail.Authenticator() {
-			@Override
-					protected PasswordAuthentication getPasswordAuthentication() {
-						return new PasswordAuthentication(username, geheim);
-					}
-				});
 		try {
 
 			for (int index = 0; index < toList.length; index++) {
@@ -85,7 +69,7 @@ public class EmailSender {
 				message.setSubject(subject);
 
 				if (asHtml) {
-					message.setContent(messageBody, "text/html; charset=utf-8");
+					message.setContent(messageBody, TEXT_HTML_CHARSET_UTF_8);
 				} else {
 					message.setText(messageBody);
 				}
@@ -97,6 +81,26 @@ public class EmailSender {
 		} catch (MessagingException e) {
 			logger.error("Error send e-mail.", e);
 		}
+	}
+
+	private static Properties createProperties() {
+		Properties properties = new Properties();
+		properties.put(SMTP_HOST_PROPERTYNAME, SMTP_HOST);
+		properties.put(SMTP_PORT_PROPERTYNAME, SMTP_PORT);
+		properties.put(SMTP_AUTH_PROPERTYNAME, SMTP_AUTH);
+
+		return properties;
+	}
+
+	private static Session createSession() {
+		Properties props = createProperties();
+		return Session.getInstance(props,
+				new javax.mail.Authenticator() {
+					@Override
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(USERNAME, GEHEIM);
+					}
+				});
 	}
 
 }
