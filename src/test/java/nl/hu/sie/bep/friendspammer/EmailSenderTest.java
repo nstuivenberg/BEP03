2bcd3d2d214b8cde757f55c50068a02d22f96e80
nick.stuivenberg <nick.stuivenberg@cgi.com>
2019-05-26 18:52:29 +0200

    opdracht04-02: Committed failing test

---
+++ src/test/java/nl/hu/sie/bep/friendspammer/EmailSenderTest.java
@@ -0,0 +1,12 @@
+package nl.hu.sie.bep.friendspammer;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+
+public class EmailSenderTest {
+
+    @Test
+    public void test_fail() {
+        fail("You shall not succeed!");
+    }
+}
